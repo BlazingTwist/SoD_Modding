289a3707f1d971329e690bc8fceb4fd2e70bed69
BlazingTwist <m.h.Freedom2@web.de>
2021-04-17 20:41:45 +0200

    refactoring

--- SoD_Enable_Logging/SodEnableLogging.cs
+++ SoD_Enable_Logging/SodEnableLogging.cs
@@ -9,7 +9,7 @@ namespace SoD_Enable_Logging
 {
 	[PublicAPI]
 	[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-	class SodEnableLogging : BaseUnityPlugin
+	public class SodEnableLogging : BaseUnityPlugin
 	{
 		public const string pluginGuid = "blazingtwist.enablelogging";
 		public const string pluginName = "BlazingTwist SoD EnableLogging";
@@ -18,21 +18,6 @@ namespace SoD_Enable_Logging
 		public static readonly string basePath = Application.dataPath + "/BlazingTwist/";
 
 		public void Awake() {
-			/*if(enableServiceCallLogging.Value) {
-				filteredServiceCalls = Config.Bind(
-					"Logging",
-					"FilteredServiceCalls",
-					string.Join(", ", new[]{
-						WsServiceType.GET_STORE,
-						WsServiceType.GET_COMMON_INVENTORY,
-						WsServiceType.GET_ALL_RANKS,
-						WsServiceType.GET_DETAILED_CHILD_LIST,
-						WsServiceType.GET_USER_UPCOMING_MISSION_STATE,
-						WsServiceType.GET_USER_ACTIVE_MISSION_STATE
-					}.Select(type => type.ToString())),
-					"List of the ServiceCalls to ignore when logging.");
-			}*/
-
 			Harmony harmony = new Harmony(pluginGuid);
 			harmony.PatchAll();
 			RuntimePatcherUtils.RunPatchers(Logger, harmony, RuntimePatcherUtils.GetTypesInNamespace(Assembly.GetExecutingAssembly(), "SoD_Enable_Logging.AsmFirstpass"));
